c3a08669dbc95be677977f9d61ffaadabdd7772c
Lu Bai <bettybowielu@gmail.com>
2022-11-22 18:47:43 -0800

    modified javascript

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # challenge-3
+
+## Description
+
+This is the UW Boot Camp Week 3 Challenge. Student was asked to 
+
+## Usage
+
+When the page is loaded, the header contains the name on the top left corner and the navigation bar on the right. When the link on the navigation bar is clicked, the page will automatically scroll to the corresponding section. When resume is clicked, it will automatically load the resume in a new tab.
+
+In the Work section, the first work application is larger than the rest of the applications. When the mouse is hovering over the application images, the image will become clearer. Clicking on the application image will lead to the application's deployment page.
+
+When the page resizes on various screens, the layout will respond accordingly to adapt to the viewport.
+
+## Visual
+
+## Links
+
+Solution URL: [https://github.com/bettybowie/challenge-3]
+
+Live URL: [https://bettybowie.github.io/challenge-3/]
+
+## License
+
+MIT License
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +14,95 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// My codes
+
+// define variables
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialChar ="!@#$%^&*(){}[]=<>/,.";
+
+// define prompts
+var confirmLength = [];
+var confirmUpper;
+var confirmLower;
+var confirmNumber;
+var confirmSpecialChar;
+
+function generatePassword() {
+  var confirmLength = prompt("Enter desired password length (between 8-128 characters");
+
+  // If a number outside the parameter is entered
+  while(confirmLength>=128 || confirmLength<=8){
+    alert("Please input a number between 8-128!");
+    var confirmLength = prompt("Enter desired password length (between 8-128 characters");
+  };
+
+  var confirmUpper = confirm("Would you like to include uppercase letters?");
+  var confirmLower = confirm("Would you like to include lowercase letters?");
+  var confirmNumber = confirm("Would you like to include numbers?");
+  var confirmSpecialChar = confirm("Would you like to include special characters?");
+
+  // If none of the criteria was picked
+  while(confirmUpper===false && confirmLower===false && confirmNumber===false && confirmSpecialLetters===false) {
+    alert("Please make sure to select OK to at least one of the criteria.")
+    var confirmUpper = confirm("Would you like to include uppercase letters?");
+    var confirmLower = confirm("Would you like to include lowercase letters?");
+    var confirmNumber = confirm("Would you like to include numbers?");
+    var confirmSpecialChar = confirm("Would you like to include special characters?");
+  };
+
+
+  // Confirming the criteria the user selected
+ var confirmMessage = "You wanted a " + confirmLength + "-digit password, containing";
+ 
+ if (confirmUpper){
+  confirmMessage += " uppercase letters,"
+ };
+
+ if (confirmLower){
+  confirmMessage += " lowercase letters,"
+ };
+
+ if (confirmNumber){
+  confirmMessage += " numbers,"
+ };
+
+ if (confirmSpecialChar){
+  confirmMessage += " special characters,"
+ };
+
+confirmMessage = confirmMessage.slice(0, -1) + '.';
+ 
+ alert(confirmMessage);
+
+//  Combining selected criteria
+var  passwordParameter = [];
+
+if (confirmUpper) {
+  passwordParameter += uppercase
+};
+
+if (confirmLower) {
+  passwordParameter += lowercase
+};
+
+if (confirmNumber) {
+  passwordParameter += numbers
+};
+
+if (confirmSpecialChar) {
+  passwordParameter += specialChar
+};
+
+// to generate random string
+var randomPassword = [];
+
+for (var i=0; i<confirmLength; i++) {
+  randomPassword = randomPassword + passwordParameter[Math.floor(Math.random() * passwordParameter.length)];
+}
+return randomPassword;
+}
+
